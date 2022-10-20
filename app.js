d4ae780b69144459a9e8ecc4ce3764c4544b1d30
iSushaant <kumar.su5hant@yahoo.com>
2022-10-20 04:22:11 -0400

    using switch statement instead of if-else to show the day instead of only weekend or weekday

--- app.js
+++ app.js
@@ -13,35 +13,35 @@ app.get("/", (req, res)=> {
     var currentDay = today.getDay();
     var day ="";
 
-    if(currentDay === 0 || currentDay === 6){
-        day = "Weekend";
-    }
-    else{
-        day = "Weekday";
-    }
+    // if(currentDay === 0 || currentDay === 6){
+    //     day = "Weekend";
+    // }
+    // else{
+    //     day = "Weekday";
+    // }
 
-//     switch (currentDay) {
-//         case 0:
-//           day = "Sunday";
-//           break;
-//         case 1:
-//           day = "Monday";
-//           break;
-//         case 2:
-//            day = "Tuesday";
-//           break;
-//         case 3:
-//           day = "Wednesday";
-//           break;
-//         case 4:
-//           day = "Thursday";
-//           break;
-//         case 5:
-//           day = "Friday";
-//           break;
-//         case 6:
-//           day = "Saturday";
-//       }
+    switch (currentDay) {
+        case 0:
+          day = "Sunday";
+          break;
+        case 1:
+          day = "Monday";
+          break;
+        case 2:
+           day = "Tuesday";
+          break;
+        case 3:
+          day = "Wednesday";
+          break;
+        case 4:
+          day = "Thursday";
+          break;
+        case 5:
+          day = "Friday";
+          break;
+        case 6:
+          day = "Saturday";
+      }
     
       res.render("list", {kindOfDay: day});
 });
