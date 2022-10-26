321d249a2e0c3d4c15626b71d3f477d45ec2fec2
iSushaant <kumar.su5hant@yahoo.com>
2022-10-26 01:52:12 -0400

    added a new method for date formatting - toLocaleDateString()

--- app.js
+++ app.js
@@ -9,43 +9,17 @@ app.set('view engine', 'ejs');
 
 app.get("/", (req, res)=> {
     
-    var today = new Date();
-    var currentDay = today.getDay();
-    var day ="";
+var today = new Date();
 
-    // if(currentDay === 0 || currentDay === 6){
-    //     day = "Weekend";
-    // }
-    // else{
-    //     day = "Weekday";
-    // }
-
-    switch (currentDay) {
-        case 0:
-          day = "Sunday";
-          break;
-        case 1:
-          day = "Monday";
-          break;
-        case 2:
-           day = "Tuesday";
-          break;
-        case 3:
-          day = "Wednesday";
-          break;
-        case 4:
-          day = "Thursday";
-          break;
-        case 5:
-          day = "Friday";
-          break;
-        case 6:
-          day = "Saturday";
-      }
-    
+var options = {
+  weekday : 'long',
+  day : 'numeric',
+  month : 'long'
+};  
+    var day = today.toLocaleDateString("en-US", options);
       res.render("list", {kindOfDay: day});
 });
 
-app.listen(3000, ()=>{
+app.listen(3000, ()=>{ 
     console.log("Server is running on port 3000");
 });
