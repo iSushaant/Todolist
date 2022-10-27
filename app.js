51ddce890de70f392e2c6457e40c9efcb9ef236e
iSushaant <kumar.su5hant@yahoo.com>
2022-10-27 00:04:20 -0400

    made a post request and sent data to the user via bodyParser

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
+var items = ["buy Food", "Cook Food", "Eat Food"];
+
 app.get("/", (req, res)=> {
-    
+
 var today = new Date();
+
 
 var options = {
   weekday : 'long',
@@ -17,7 +20,15 @@ var options = {
   month : 'long'
 };  
     var day = today.toLocaleDateString("en-US", options);
-      res.render("list", {kindOfDay: day});
+      res.render("list", {kindOfDay: day, newListItems: items});
+});
+
+app.post("/", (req, res)=> {
+ item = req.body.newItem;
+
+ items.push(item);
+
+ res.redirect("/");
 });
 
 app.listen(3000, ()=>{ 
