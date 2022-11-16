dcb86dee95b14f4891bcd1dfe252b640893c9469
iSushaant <kumar.su5hant@yahoo.com>
2022-11-16 12:09:10 +0530

    added a new page - Work List

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
+app.use(express.static('public'));
+
 let items = ["buy Food", "Cook Food", "Eat Food"];
 let workItems = [];
 
@@ -21,15 +23,24 @@ let options = {
   month : 'long'
 };  
     let day = today.toLocaleDateString("en-US", options);
-      res.render("list", {kindOfDay: day, newListItems: items});
+      res.render("list", {listTitle: day, newListItems: items});
 });
 
 app.post("/", (req, res)=> {
- let item = req.body.newItem;
 
- items.push(item);
+  let item = req.body.newItem;
 
- res.redirect("/");
+  if(req.body.list === "Work"){
+    workItems.push(item);
+    res.redirect("/work");
+  } else{
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", (req, res)=>{
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
 });
 
 app.listen(3000, ()=>{ 
